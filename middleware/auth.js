9c90bc0b6ba1f221b30a4d0e88f675cd9d422f6e
belccarvalho <belccarvalho@gmail.com>
2021-01-08 18:07:01 +0100

    token auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,20 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
+
 module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header("x-auth-token");
 
-  // Check if not token
+  // Check if no token
   if (!token) {
-    return;
-    res.status(401).json({ msg: "No token, authorization denied" });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   //Verify token
   try {
     //verify() function decodes token and return an object. This object contains another object called "user" which contains the _id from MongoDB
     const decoded = jwt.verify(token, config.get("jwtSecret"));
-    res.user = decoded.user; //→ req.user contains the id from mongoDb Object.
+    req.user = decoded.user; //→ req.user contains the id from mongoDb Object.
     next();
   } catch (error) {
     res.status(401).json({ msg: "Token is not valid" });
