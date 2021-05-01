433f80186759b0cd2e641a280a40be0ebcd82223
belccarvalho <belccarvalho@gmail.com>
2021-05-01 11:02:07 +0200

    delete comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,12 +5,14 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 import { getPost } from "../../actions/post";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost]);
+
   return loading || post === null ? (
     <Spinner />
   ) : (
@@ -20,6 +22,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false}></PostItem>
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>{" "}
     </Fragment>
   );
 };
